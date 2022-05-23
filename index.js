75e94a67a5cbd2cb9194f6b1628b07ca8e0af106
Nick <nbuscem1@binghamton.edu>
2022-05-23 11:56:18 -0400

    commit tree a5b9e9e31d258c24e7c56ff137913d47d7de5abd

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun("play basketball"))
+console.log(saturdayFun())
+
+function mondayWork (def = "go to the office") {
+    return `This Monday, I will ${def}.`;
+}
+console.log(mondayWork())
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+console.log(wrapAdjective("%")("a dedicated programmer"));
